8b4fd3ec8d4b0f8f1911bf98d04dae2e0056500a
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-04-18 20:29:03 +0200

    hangfire configuration

--- TrananMVC/Program.cs
+++ TrananMVC/Program.cs
@@ -1,46 +1,117 @@
+using Hangfire;
+using Hangfire.MemoryStorage;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+GlobalConfiguration.Configuration.UseMemoryStorage();
+
+builder.Services.AddHangfire(config =>
+{
+    config.UseMemoryStorage();
+});
+
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Core.Data.TrananDbContext>(options =>
-    options.UseSqlite("Data Source=c:\\Users\\angel\\Documents\\SUVNET22\\OOP2\\INLÄMNINGAR\\bio-tranan-Radagastno1\\Core\\tranandatabase.db"));
+builder.Services.AddDbContext<Core.Data.TrananDbContext>(
+    options =>
+        options.UseSqlite(
+            "Data Source=c:\\Users\\angel\\Documents\\SUVNET22\\OOP2\\INLÄMNINGAR\\bio-tranan-Radagastno1\\Core\\tranandatabase.db"
+        )
+);
 
-builder.Services.AddScoped<Core.Interface.IRepository<Core.Models.Movie>,Core.Data.Repository.MovieRepository>();
-builder.Services.AddScoped<Core.Interface.IRepository<Core.Models.MovieScreening>,Core.Data.Repository.MovieScreeningRepository>();
-builder.Services.AddScoped<Core.Interface.IRepository<Core.Models.Reservation>,Core.Data.Repository.ReservationRepository>();
-builder.Services.AddScoped<Core.Interface.IRepository<Core.Models.Theater>,Core.Data.Repository.TheaterRepository>();
-builder.Services.AddScoped<Core.Interface.IActorRepository,Core.Data.Repository.ActorRepository>();
-builder.Services.AddScoped<Core.Interface.ISeatRepository,Core.Data.Repository.SeatRepository>();
-builder.Services.AddScoped<Core.Interface.IRepository<Core.Models.Review>,Core.Data.Repository.ReviewRepository>();
-builder.Services.AddScoped<Core.Interface.IMovieScreeningRepository,Core.Data.Repository.MovieScreeningRepository>();
-builder.Services.AddScoped<Core.Interface.IReservationRepository,Core.Data.Repository.ReservationRepository>();
+builder.Services.AddScoped<
+    Core.Interface.IRepository<Core.Models.Movie>,
+    Core.Data.Repository.MovieRepository
+>();
+builder.Services.AddScoped<
+    Core.Interface.IRepository<Core.Models.MovieScreening>,
+    Core.Data.Repository.MovieScreeningRepository
+>();
+builder.Services.AddScoped<
+    Core.Interface.IRepository<Core.Models.Reservation>,
+    Core.Data.Repository.ReservationRepository
+>();
+builder.Services.AddScoped<
+    Core.Interface.IRepository<Core.Models.Theater>,
+    Core.Data.Repository.TheaterRepository
+>();
+builder.Services.AddScoped<Core.Interface.IActorRepository, Core.Data.Repository.ActorRepository>();
+builder.Services.AddScoped<Core.Interface.ISeatRepository, Core.Data.Repository.SeatRepository>();
+builder.Services.AddScoped<
+    Core.Interface.IRepository<Core.Models.Review>,
+    Core.Data.Repository.ReviewRepository
+>();
+builder.Services.AddScoped<
+    Core.Interface.IMovieScreeningRepository,
+    Core.Data.Repository.MovieScreeningRepository
+>();
+builder.Services.AddScoped<
+    Core.Interface.IReservationRepository,
+    Core.Data.Repository.ReservationRepository
+>();
 
-builder.Services.AddScoped<Core.Interface.IService<Core.Models.Movie>,Core.Services.MovieService>();
-builder.Services.AddScoped<Core.Interface.IService<Core.Models.MovieScreening>,Core.Services.MovieScreeningService>();
-builder.Services.AddScoped<Core.Interface.IService<Core.Models.Theater>,Core.Services.TheaterService>();
+builder.Services.AddScoped<
+    Core.Interface.IService<Core.Models.Movie>,
+    Core.Services.MovieService
+>();
+builder.Services.AddScoped<
+    Core.Interface.IService<Core.Models.MovieScreening>,
+    Core.Services.MovieScreeningService
+>();
+builder.Services.AddScoped<
+    Core.Interface.IService<Core.Models.Theater>,
+    Core.Services.TheaterService
+>();
 builder.Services.AddScoped<Core.Services.SeatService>();
-builder.Services.AddScoped<Core.Interface.IService<Core.Models.Reservation>,Core.Services.ReservationService>();
-builder.Services.AddScoped<Core.Interface.IService<Core.Models.Review>,Core.Services.ReviewService>();
-builder.Services.AddScoped<Core.Interface.IMovieScreeningService,Core.Services.MovieScreeningService>();
-builder.Services.AddScoped<Core.Interface.IReservationService,Core.Services.ReservationService>();
-builder.Services.AddScoped<Core.Interface.IReviewService,Core.Services.ReviewService>();
+builder.Services.AddScoped<
+    Core.Interface.IService<Core.Models.Reservation>,
+    Core.Services.ReservationService
+>();
+builder.Services.AddScoped<
+    Core.Interface.IService<Core.Models.Review>,
+    Core.Services.ReviewService
+>();
+builder.Services.AddScoped<
+    Core.Interface.IMovieScreeningService,
+    Core.Services.MovieScreeningService
+>();
+builder.Services.AddScoped<Core.Interface.IReservationService, Core.Services.ReservationService>();
+builder.Services.AddScoped<Core.Interface.IReviewService, Core.Services.ReviewService>();
 
-builder.Services.AddScoped<TrananMVC.Interface.IMovieService<TrananMVC.ViewModel.MovieViewModel>,TrananMVC.Service.MovieService>();
-builder.Services.AddScoped<TrananMVC.Interface.IMovieService<TrananMVC.ViewModel.MovieScreeningViewModel>,TrananMVC.Service.MovieScreeningService>();
-builder.Services.AddScoped<TrananMVC.Interface.IReservationService,TrananMVC.Service.ReservationService>();
-builder.Services.AddScoped<TrananMVC.Interface.IMovieTrailerService,TrananMVC.ApiServices.MovieTrailerService>();
-builder.Services.AddScoped<TrananMVC.Interface.IReviewService,TrananMVC.Service.ReviewService>();
+builder.Services.AddScoped<
+    TrananMVC.Interface.IMovieService<TrananMVC.ViewModel.MovieViewModel>,
+    TrananMVC.Service.MovieService
+>();
+builder.Services.AddScoped<
+    TrananMVC.Interface.IMovieService<TrananMVC.ViewModel.MovieScreeningViewModel>,
+    TrananMVC.Service.MovieScreeningService
+>();
+builder.Services.AddScoped<
+    TrananMVC.Interface.IReservationService,
+    TrananMVC.Service.ReservationService
+>();
+builder.Services.AddScoped<
+    TrananMVC.Interface.IMovieTrailerService,
+    TrananMVC.ApiServices.MovieTrailerService
+>();
+builder.Services.AddScoped<TrananMVC.Interface.IReviewService, TrananMVC.Service.ReviewService>();
+
+builder.Services.AddScoped<Core.Workers.HangfireWorker>();
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+
+GlobalConfiguration.Configuration.UseActivator(new Core.Workers.HangfireActivator(serviceProvider));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseHangfireDashboard();
+app.UseHangfireServer();
+
+serviceProvider.GetService<Core.Workers.HangfireWorker>().DailyWork();
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Bio/Error");
     app.UseHsts();
 }
 
